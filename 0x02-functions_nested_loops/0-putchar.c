5bcbdb2ec0756cb544830162068c2222fd7fbf01
DeraOnyekachi <chizzukuzi@gmail.com>
2022-04-12 15:27:53 -0700

    my commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
-#include "main.h" 
+#include "main.h"
 
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
 
 int main(void)
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -12,7 +17,7 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n'); 
+_putchar('\n');
 
-return 0;
+return (0);
 }
